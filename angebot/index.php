0247e4f1220d2478ed2a1339655862f50a1473a8
dmodliba <dmodliba@student.tgm.ac.at>
2017-12-19 10:34:12 +0100

    First commit

---
+++ angebot/index.php
@@ -0,0 +1,221 @@
+<html>
+<head>
+	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+	<title> Test </title>
+</head>
+<body>
+
+	<nav class="navbar navbar-default" role="navigation">
+	
+	  <div class="navbar-header">
+		<button type="button" class="navbar-toggle">
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		</button>    
+	  </div>
+	  
+		<div class="navbar-collapse collapse">
+		
+			<ul class="nav navbar-nav">
+				<li><a href="#"><img src="logo.jpg" width="30" height="30"></a></li>
+				<li><a href="#">UTSS Titan Deutsch Wagram</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#team">Team</a></li>
+				<li><a href="#news">News</a></li>
+				<li><a href="#angebot">Angebot</a></li>
+				<li><a href="#contact">Kontakt</a></li>
+			</ul>
+			
+		</div>
+	</nav>
+
+<div class="container">
+
+<h1 align="center">UNSER ANGEBOT</h1>
+
+ 
+	<div class="row">
+		<div class="col-md-12">
+			<img style="width:100%" id="image" src="eins.jpg">
+			</br></br>
+			</br></br>
+		</div>
+	</div>
+
+
+
+
+
+
+	<div class="row">
+
+		<div class="col-md-6">
+			<p font="Open Sans", align="justify">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+				<br></br>
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				<br></br>
+			</p>
+		</div>
+
+		<div class="col-md-6">
+			<img src="zwei.jpg"></img>
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+ 
+	<div class="row">
+		<div class="col-md-12">
+			<img style="width:100%" id="image" src="eins.jpg">
+			</br></br>
+			</br></br>
+		</div>
+	</div>
+
+
+
+
+
+	<div class="row">
+
+		<div class="col-md-6">
+			<img src="zwei.jpg"></img>
+		</div>
+
+		<div class="col-md-6">
+			<p font="Open Sans", align="justify">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+				<br></br>
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				<br></br>
+			</p>
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+ 
+	<div class="row">
+		<div class="col-md-12">
+			<img style="width:100%" id="image" src="eins.jpg">
+			</br></br>
+			</br></br>
+		</div>
+	</div>
+
+
+
+
+
+
+	
+	<div class="row">
+
+		<div class="col-md-6">
+			<p font="Open Sans", align="justify">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+				<br></br>
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+				<br></br>
+			</p>
+		</div>
+
+		<div class="col-md-6">
+			<img src="zwei.jpg"></img>
+		</div>
+
+	</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	<div class="container text-center">
+    <hr />
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="col-md-4">
+        <ul class="nav nav-pills nav-stacked">
+		  <li><h4>Administrator</h4></li>
+          <li><a href="#">Log In</a></li>
+          <li><a href="#">Neuer Eintrag</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <ul class="nav nav-pills nav-stacked">
+		  <li><h4>Informationen</h4></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Angebot</a></li>
+		  <li><a href="#">&Uuml;ber uns</a></li>
+		  <li><a href="#">Freie Tanzpartner</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <ul class="nav nav-pills nav-stacked">
+		  <li><h4>Details</h4></li>
+          <li><a href="#">Kontakt</a></li>
+          <li><a href="#">Impressum</a></li>          
+        </ul>
+      </div>
+    </div>
+  </div>
+  <hr>
+    <div class="row">
+        <div class="col-lg-12">
+			<div class="col-md-4">
+				<ul class="nav nav-pills nav-justified">
+					<li><a href="/">&copy; 2017 - UTSS Titan Deutsch Wagram</a></li>
+				</ul>
+			</div>
+        </div>
+    </div>
+</div>
+	
+	
+	
+	
+	
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,17 @@
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<h1>
+							Footer
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>

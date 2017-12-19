c608deaaa96879acb85b94dc83cbe2a9a1a459e1
dmodliba <dmodliba@student.tgm.ac.at>
2017-12-19 16:05:38 +0100

    ordnerstruktur

--- header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-		
-		<link rel="stylesheet" href="<?= get_bloginfo('template_directory'); ?>/style.css">
-		<?php wp_head(); ?>
-	</head>
-	<body>
